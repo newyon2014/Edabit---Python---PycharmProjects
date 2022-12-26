27c2ea6eadb34a308828eb5e529f8bfa927cb18f
newyon2014 <yon2005_t@yahoo.com>
2022-12-26 13:27:08 -0500

    12/26 - added more python solution

---
+++ Easy/ANDComparedToOR.py
@@ -0,0 +1,12 @@
+def AND(num, num2):
+    if num == 1 and num2 == 1:
+        return 1
+    else:
+        return 0
+
+
+def OR(num, num2):
+    if num == 1 or num2 == 1:
+        return 1
+    else:
+        return 0

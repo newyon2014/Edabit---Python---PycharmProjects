a04543236de967c1e922cc2f7ca4e751d83fcbcc
newyon2014 <yon2005_t@yahoo.com>
2023-02-08 17:33:50 -0500

    002/08 - Added more python solution

---
+++ Easy/MinimalBooleanRedundancyII.py
@@ -0,0 +1,7 @@
+def parity(n):
+    # remainder = bool(n % 2)
+    # if remainder == False:
+    # return "even"
+    # if remainder == True:
+    # return "odd"
+    return "even" if n % 2 == 0 else "odd"

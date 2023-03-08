1e39835b64fcc0186de5593b952f9a08a1286d0c
newyon2014 <yon2005_t@yahoo.com>
2023-03-08 16:27:22 -0500

    03/08 - added more python coding solution

---
+++ Easy/WordNumbers.py
@@ -0,0 +1,4 @@
+def word(s):
+    word_dict = {"one": 1, "two": 2, "three": 3, "four": 4, "five": 5, "six": 6, "seven": 7, "eight": 8, "nine": 9,
+                 "zero": 0}
+    return word_dict.get(s)

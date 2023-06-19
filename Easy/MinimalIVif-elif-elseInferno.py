3a630eb462ea23798da2417abb5150132474b1f3
newyon2014 <yon2005_t@yahoo.com>
2023-06-19 17:59:13 -0400

    06/19 - added more python solution

---
+++ Easy/MinimalIVif-elif-elseInferno.py
@@ -0,0 +1,8 @@
+	def equilibrium(x):
+  return "positive" if x > 0 else "negative" if x < 0 else True
+	#if x > 0:
+	#	return "positive"
+	#elif x < 0:
+	#	return "negative"
+	#else:
+	#	return True
